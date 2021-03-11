2c21475f66af3b6cb147413dfb3120e95fec7739
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-11 17:30:41 -0600

    Import framer dependencies

--- src/pages/Jobpage/Jobpage.js
+++ src/pages/Jobpage/Jobpage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./style.css";
+import { Frame, useMotionValue, useTransform, useAnimation } from "framer";
 
 function Jobpage() {
+
     return(
         <div className="job-card">
-            
+
         </div>
     )
 }

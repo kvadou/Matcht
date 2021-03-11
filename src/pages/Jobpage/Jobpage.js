96f7ccb0bb60ddc5ad8596a9cb3c10f86c4f97c7
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-11 17:37:57 -0600

    Create opacity value to create disappear effect when the user swipped or drag the card to the left or right

--- src/pages/Jobpage/Jobpage.js
+++ src/pages/Jobpage/Jobpage.js
@@ -8,6 +8,8 @@ function Jobpage() {
 
     const rotateVal = useTransform(motionVal, [-200, 200], [-50, 50]);
 
+    const opacityVal = useTransform(motionVal, [-200, -150, 0, 150, 200], [0, 1, 1, 1, 0]);
+
     return(
         <div className="job-card">
 

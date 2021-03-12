2d026a8a098a0df5d4b94c77d90c57293446be8e
KELSEY <klyngdal@gmail.com>
2021-03-11 19:31:30 -0600

    json install

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 
 function Header() {

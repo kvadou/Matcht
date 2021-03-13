bd29ac99344edd26736bf6b84852a3f83c3b914d
KELSEY <klyngdal@gmail.com>
2021-03-13 11:04:06 -0600

    private route for auth addition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Jobpage from "./pages/Jobpage/Jobpage";
@@ -5,24 +6,33 @@ import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile.jsx";
 import Search from "./pages/Search/Search";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Switch from "react-bootstrap/esm/Switch";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Switch from "react-bootstrap/esm/Switch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute.js";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  console.log(login);
   return (
     <Router>
       <div className="App">
-        <Login />
+        <div>
+          <p>You must log in to view the page</p>
+          <button onClick={() => setLogin(!login)}>Log in</button>
+        </div>
         <Header />
         <Switch>
-          <Route exact path="/search">
+          <PrivateRoute login={login} path="/search">
             <Search />
-          </Route>
-          <Route exact path="/jobpage">
+          </PrivateRoute>
+          <PrivateRoute login={login} path="/jobpage">
             <Jobpage />
-          </Route>
-          <Route exact path="/profile">
+          </PrivateRoute>
+          <PrivateRoute login={login} path="/profile">
             <Profile />
+          </PrivateRoute>
+          <Route path="/">
+            <Login />
           </Route>
         </Switch>
       </div>

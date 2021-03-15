37364278d69b6e88de609c140ac8a43de2db4713
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-15 18:48:35 -0500

    Added the class name and key to the swipe card

--- client/src/pages/Jobpage/Jobpage.js
+++ client/src/pages/Jobpage/Jobpage.js
@@ -3,26 +3,26 @@ import SwipeCard from "react-tinder-card";
 import "./style.css";
 
 class Jobpage extends Component {
-
   state = {
     results: [],
   };
 
   componentDidMount() {
     fetch("https://random-data-api.com/api/company/random_company?size=10")
-      .then((response) => response.json())  
+      .then((response) => response.json())
       .then((res) => this.setState({ results: res }))
       .catch((err) => console.log(err));
-  };
+  }
 
   render() {
     return (
       <div className="container">
         <div className="job-card">
           {this.state.results.map((job) => (
-            <SwipeCard >
-              
-            </SwipeCard>
+            <SwipeCard 
+              className="swipe" 
+              key={job.business_name}
+            ></SwipeCard>
           ))}
         </div>
       </div>

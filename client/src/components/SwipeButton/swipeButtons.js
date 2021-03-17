35b18d6314e9f17bf8eab78fac24e4a78f70ac4c
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-17 10:01:37 -0500

    Create the swipe button component

---
+++ client/src/components/SwipeButton/swipeButtons.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function SwipeButtons() {
+  return (
+    <div className="swipeButtons">
+
+    </div>
+  );
+}
+
+export default SwipeButtons;

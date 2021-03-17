ae36ab2b55848fc64fcca5a5338b45e382ee1e2a
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-17 09:52:48 -0500

    Create a route to get, post, and delete data

---
+++ routes/api/jobs.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const jobsController = require("../../controllers/Controller");
+
+router.route("/jobpage")
+  .get(jobsController.findAll)
+  .post(jobsController.create);
+
+router
+  .route("/:id")
+  .get(jobsController.findById)
+  .delete(jobsController.remove);
+
+module.exports = router;

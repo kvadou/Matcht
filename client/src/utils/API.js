6012ef278c8c3b977ea8bb4ed38f4c6c0f66b622
DPK <dougkvamme@gmail.com>
2021-03-18 18:23:46 -0500

    updated jobpage

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 const jobAPI = {
-    getJobs: function() {
-        return axios.get("/api/jobpage");
-    },
-    getJob: function(id) {
-         return axios.get("/api/jobpage/" + id);
-    },
-    deleteJob: function(id) {
-        return axios.delete("/api/jobpage/" + id);
-    }
+  getJobs: function () {
+    return axios.get("/api/jobpage/jobpage");
+  },
+  getJob: function (id) {
+    return axios.get("/api/jobpage/" + id);
+  },
+  deleteJob: function (id) {
+    return axios.delete("/api/jobpage/" + id);
+  },
 };
 
 export default jobAPI;

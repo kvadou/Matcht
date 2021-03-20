f766b623fbf1f99b41f330f2c05d146a463bccdd
KELSEY <klyngdal@gmail.com>
2021-03-20 14:18:49 -0500

    search location css

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -3,6 +3,7 @@ import "./Search.css";
 import API from "../../utils/API";
 //import JobTable from "../../components/JobTable/JobTable";
 import SwipeCard from "react-tinder-card";
+import { Form, Col } from "react-bootstrap";
 
 class Search extends Component {
   state = {
@@ -34,55 +35,54 @@ class Search extends Component {
 
   render() {
     return (
-      <div class="search">
-        <form>
-          <label class="jobs">
-            <span className="visually-hidden">Search Location</span>
-          </label>
-          <input
-            className="m-1"
-            type="search"
-            id="header-search"
-            placeholder="Location"
-            name="location"
-            onChange={this.handleInputLocationChange.bind(this)}
-          />
-        </form>
+      <Form>
         <div className="container">
-          <div className="job-card">
-            {this.state.jobData.map((res) => (
-              <SwipeCard
-                className="swipe"
-                key={res._id}
-                preventSwipe={["up", "down"]}
-              >
-                <div className="row">
-                  <div className="offset-3 col-6 offset-3">
-                    <div className="jumbotron jumbotron-fluid">
-                      <div className="container">
-                        <h4 className="display-3"> {res.title} </h4>
-                        <h6>Type: {res.job_type}</h6>
-                        <h6>Company: {res.company_name}</h6>
-                        <h6>Category: {res.category}</h6>
-                        <h6>
-                          {" "}
-                          Required Location: {res.candidate_required_location}
-                        </h6>
-                        <h6>
-                          {" "}
-                          <a href={res.url}> Link to Posting </a>{" "}
-                        </h6>
-                        <hr className="my-4" />
-                        <p>{res.description} </p>
-                      </div>
-                    </div>
+          <div class="row search">
+            <Form.Group>
+              <Form.Row>
+                <Form.Label className="search-header">Search Location </Form.Label>
+                <Col>
+                  <Form.Control
+                    className="m-1"
+                    type="search"
+                    placeholder="Country"
+                    onChange={this.handleInputLocationChange.bind(this)}
+                  />
+                </Col>
+              </Form.Row>
+              <Form.Row>
+                <div className="container">
+                  <div className="row job-card">
+                    {this.state.jobData.map((res) => (
+                      <SwipeCard className="swipe" key={res._id} preventSwipe={["up", "down"]}>
+                        <div className="row">
+                          <div className="offset-3 col-6 offset-3">
+                            <div className="jumbotron jumbotron-fluid">
+                              <div className="container">
+                                <h4 className="display-3"> {res.title} </h4>
+                                <h6>Type: {res.job_type}</h6>
+                                <h6>Company: {res.company_name}</h6>
+                                <h6>Category: {res.category}</h6>
+                                <h6> Required Location: {res.candidate_required_location}</h6>
+                                <h6>
+                                  {" "}
+                                  <a href={res.url}> Link to Posting </a>{" "}
+                                </h6>
+                                <hr className="my-4" />
+                                <p>{res.description} </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </SwipeCard>
+                    ))}
                   </div>
                 </div>
-              </SwipeCard>
-            ))}
+              </Form.Row>
+            </Form.Group>
           </div>
         </div>
-      </div>
+      </Form>
     );
   }
 }

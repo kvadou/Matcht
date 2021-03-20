291c9ca07b0bad4853c4f22da91e5b13f8444cb8
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-19 23:58:34 -0500

    Added save route to post the data

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,9 +7,12 @@ const jobAPI = {
   getJob: function (id) {
     return axios.get("/api/jobpage/" + id);
   },
+  saveJob: function(jobData) {
+    return axios.post("/api/jobpage", jobData);
+  },
   deleteJob: function (id) {
     return axios.delete("/api/jobpage/" + id);
-  },
+  }
 };
 
 export default jobAPI;

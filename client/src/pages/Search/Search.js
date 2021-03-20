ae84f88637f9e578fa597b0ee233787e66f3a980
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2021-03-19 23:58:02 -0500

    Set up the search job by location

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,23 +1,57 @@
 import React, { Component } from "react";
 import "./Search.css";
+import API from "../../utils/API";
+import JobTable from "../../components/JobTable/JobTable";
 
 class Search extends Component {
-  handleClick() {
-    console.log("Click happened");
+
+  state = {
+    jobData: [],
+    locationSearched: []
+  };
+
+  componentDidMount() {
+    this.loadJobData();
   }
+
+  loadJobData = () => {
+    API.getJobs()
+      .then((res) => {
+        this.locationSearched = res.data;
+        this.setState({ jobData: res.data })
+      })
+      .catch((err) => console.log(err));
+  }
+
+  handleInputLocationChange = event => {
+    let newJobs = this.locationSearched.filter((res) => {
+      console.log(res.candidate_required_location)
+      let searchValue = res.data.candidate_required_location.toLowerCase();
+      return searchValue.indexOf(event.target.value) !== -1;
+    });
+    this.setState({ jobData:newJobs })
+  };
+
   render() {
     return (
       <div class="search">
-        <form action="/jobsearch" method="get">
+        <form>
           <label class="jobs">
             <span className="visually-hidden">Search Location</span>
           </label>
-          <input className="m-1" type="text" id="header-search" size="lg" placeholder="Search Jobs" name="jobname" />
-          <input className="m-1" type="text" id="header-search" placeholder="Location" name="location" />
-          <button className="btn btn-info" type="submit" onClick={this.handleClick}>
-            Find Jobs
-          </button>
+          <input className="m-1" type="search" id="header-search" placeholder="Location" name="location" onChange={() => this.handleInputLocationChange.bind(this)}/>
         </form>
+        {this.state.jobData.map((res) => (
+          <JobTable 
+            title={res.data.title}
+            job_type={res.data.job_type}
+            company_name={res.data.company_name}
+            category={res.data.category}
+            candidate_required_location={res.data.candidate_required_location}
+            url={res.data.url}
+            description={res.data.description}
+          />
+        ))}
       </div>
     );
   }
